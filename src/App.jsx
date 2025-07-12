ed77bca48a37ddc3a76da2bf9981ed6aea6492e7
Ghina Shour <shourghina@gmail.com>
2025-07-12 19:57:00 +0300

    adding red and green functionality to the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 import React from "react"
 import { languages } from "../languages"
+import { clsx } from "clsx"
 
 
 export default function AssemblyEndgame() {
   const [currentWord, setCurrentWord] = React.useState("react")
+  const [guessedLetters, setGuessedLetters] = React.useState([])
 
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-    const alphabetLetters = alphabet.split("").map((letter,index) =>(
-      <button className="alphabet"  key={index}>{letter}</button>
-    ))
+    function addGuessedLetter(letter) {
+      setGuessedLetters(prev => prev.includes(letter)? prev : [...prev, letter])
+    }
+    const alphabetLetters = alphabet.split("").map(letter => {
+        const isGuessed = guessedLetters.includes(letter)
+        const isCorrect = isGuessed && currentWord.includes(letter)
+        const isWrong = isGuessed && !currentWord.includes(letter)
+        const className = clsx({
+            correct: isCorrect,
+            wrong: isWrong
+        })
+        
+        console.log(className)
+        
+        return (
+            <button
+                className={className}
+                key={letter}
+                onClick={() => addGuessedLetter(letter)}
+            >
+                {letter.toUpperCase()}
+            </button>
+        )
+    })
+
 
     const languageElements = languages.map(lang => {
         const styles = {
@@ -47,9 +71,10 @@ export default function AssemblyEndgame() {
                 {languageElements}
             </section>
             <section className="word">{letterElements}</section>
-            <section className="alphabet-buttons">
+            <section className="alphabet">
                 {alphabetLetters}
-            </section>
+            </section> 
+               <button className="new-game">New Game</button>
         </main>
     )
 }
